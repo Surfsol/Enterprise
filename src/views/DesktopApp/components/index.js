c5533b6fe8c76c04a80a25497cbe8d3034a21308
Surfsol <surfsol@protonamil.com>
2021-01-02 18:16:57 -0300

    initial commit

---
+++ src/views/DesktopApp/components/index.js
@@ -0,0 +1,11 @@
+export { default as Customization } from './Customization';
+export { default as Download } from './Download';
+export { default as Hero } from './Hero';
+export { default as Hub } from './Hub';
+export { default as Partners } from './Partners';
+export { default as Pricings } from './Pricings';
+export { default as Reviews } from './Reviews';
+export { default as Support } from './Support';
+export { default as Datapoints } from './Datapoints';
+export { default as Usecases } from './Usecases';
+export { default as Process } from './Process';
